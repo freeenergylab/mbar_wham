ef300f2b31d6c45b769de194dd860ba329a421bc
lipengfei <lipengfei_mail@126.com>
2023-11-09 11:56:36 +0800

    only keep wham part

--- src/main.f90
+++ src/main.f90
@@ -1,18 +1,9 @@
 program main
 use wham
 use extract
-use mbar
-use mbarpmf
-use weight
 implicit none
 
 call system('mkdir -p bindexs')
 call wham2
-call system('mkdir -p energy')
-call extr
-call mbar2
-call mbarpmf2
-call system('mkdir -p weights')
-call weight2
 
 end
